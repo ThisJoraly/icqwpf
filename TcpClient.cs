3adad2f3e1b57f1319f4bbea891b840863b39961
Artyom Findin <tyomajoraly@gmail.com>
2024-05-16 22:43:18 +0300

    Initial commit

---
+++ TcpClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YourNamespace.ViewModels
+{
+    public class TcpClient
+    {
+        private Socket _clientSocket;
+        private CancellationTokenSource _cancellationTokenSource;
+        private string ipAddress { get; set; }
+        private int port { get; set; }
+
+        public event Action<string> OnMessageReceived;
+
+        public TcpClient(string ipAddress, int port)
+        {
+           this.ipAddress = ipAddress; this.port = port;
+            _cancellationTokenSource = new CancellationTokenSource();
+            _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect(string username)
+        {
+            _clientSocket.BeginConnect(new IPEndPoint(IPAddress.Parse(ipAddress), port), ConnectCallback, null);
+        }
+
+        public void Disconnect()
+        {
+            _cancellationTokenSource.Cancel();
+
+            if (_clientSocket.Connected)
+            {
+                SendMessage("/disconnect");
+                _clientSocket.Close();
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            if (_clientSocket.Connected)
+            {
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                _clientSocket.Send(data);
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult result)
+        {
+            _clientSocket.EndConnect(result);
+
+            SendMessage($"/username TODO");  //TODO username
+
+            Task.Factory.StartNew(() =>
+            {
+                ReceiveMessages();
+            }, _cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] receiveBuffer = new byte[1024];
+            int bytesReceived;
+
+            while ((bytesReceived = _clientSocket.Receive(receiveBuffer)) > 0)
+            {
+                string message = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, bytesReceived);
+
+                OnMessageReceived?.Invoke(message);
+            }
+
+            _clientSocket.Close();
+        }
+    }
+}
---
+++ TcpServer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace icqwpf
+{
+    public class TcpServer
+    {
+        private int port { get; set; }
+        private Socket _serverSocket;
+        private CancellationTokenSource _cancellationTokenSource;
+        private ConcurrentDictionary<string, Socket> _connectedClients;
+
+        public event Action<long, string> OnMessageReceived; // long -> USER model
+        public event Action<long> OnUserConnected;
+        public event Action<long> OnUserDisconnected;
+
+        public TcpServer(int port)
+        {
+            this.port = port;
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _cancellationTokenSource = new CancellationTokenSource();
+            _connectedClients = new ConcurrentDictionary<string, Socket>();
+        }
+
+        public void Start()
+        {
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            _serverSocket.Listen(10);
+
+            Task.Factory.StartNew(() =>
+            {
+                while (!_cancellationTokenSource.IsCancellationRequested)
+                {
+                    Socket clientSocket = _serverSocket.Accept();
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        HandleClient(clientSocket);
+                    }, _cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                }
+            }, _cancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+            _serverSocket.Close();
+        }
+
+        private void HandleClient(Socket clientSocket)
+        {
+            byte[] receiveBuffer = new byte[1024];
+            int bytesReceived;
+
+            while ((bytesReceived = clientSocket.Receive(receiveBuffer)) > 0)
+            {
+                string message = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, bytesReceived);
+
+                if (message.StartsWith("/username "))
+                {
+                    string username = message.Substring(9);
+                    _connectedClients[username] = clientSocket;
+/*
+                    Long user = new User
+                    {
+                        Username = username
+                    };*/
+
+ /*                   OnUserConnected?.Invoke(user);*/
+                }
+                else if (message == "/disconnect")
+                {
+                    string username = GetUsernameByClient(clientSocket);
+
+                    if (!string.IsNullOrEmpty(username))
+                    {
+                        _connectedClients.TryRemove(username, out Socket removedClient);
+/*
+                        User user = new User
+                        {
+                            Username = username
+                        };
+*//*
+                        OnUserDisconnected?.Invoke(user);*/
+                    }
+
+                    break;
+                }
+                else
+                {
+                    string username = GetUsernameByClient(clientSocket);
+/*
+                    if (!string.IsNullOrEmpty(username))
+                    {
+                        OnMessageReceived?.Invoke(new User { Username = username }, message);
+                    }*/
+                }
+            }
+
+            clientSocket.Close();
+        }
+
+        private string GetUsernameByClient(Socket clientSocket)
+        {
+            foreach (var item in _connectedClients)
+            {
+                if (item.Value.Equals(clientSocket))
+                {
+                    return item.Key;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
